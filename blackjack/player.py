9e066067fe338c5e89db75ce5dad634def292af4
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-10-18 05:35:47 +0530

    Implement player and dealer

--- blackjack/player.py
+++ blackjack/player.py
@@ -1,10 +1,43 @@
-class GenericPlayer:
-    pass
+from __future__ import annotations
 
 
-class Player(GenericPlayer):
-    pass
+from .deck import Deck
+from .hand import Hand
 
 
-class Dealer(GenericPlayer):
-    pass
+class _GenericPlayer:
+    def __init__(self) -> None:
+        self.hand = Hand()
+
+    def player_count(self, aces=None) -> int:
+        return self.hand.count(aces=aces)
+
+    def draw_card(self, deck: Deck):
+        card = deck.pick_card()
+        self.hand.add_card(card)
+
+    def has_blackjack(self, aces=None):
+        return self.player_count(aces=aces) == 21
+
+    def has_busted(self, aces=None):
+        return self.player_count(aces=aces) > 21
+
+
+class Player(_GenericPlayer):
+    def __init__(self, name: str, bankroll: float) -> None:
+        self.name = name
+        self.bankroll = bankroll
+        super().__init__()
+
+    def pay(self, amount: float, multiplier=1):
+        self.bankroll += amount * multiplier
+
+    def bet(self, amount: float):
+        self.bankroll -= amount
+
+
+class Dealer(_GenericPlayer):
+    def __init__(self) -> None:
+        self.face_up = ""
+        self.face_down = ""
+        super().__init__()
