24a49ef26b64ba234a146ea1d1a881a2ea8eb6e1
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-10-18 05:35:30 +0530

    Make count completely dynamic

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -1,13 +1,26 @@
+from __future__ import annotations
+
 import functools
 from typing import Dict, List
 
 from .deck import FACE_CARDS
 
 
+class CountingStrategyMissingError(ValueError):
+    """No counting strategy has been specified even though aces are present."""
+
+
+class UnRecognizedCountingStrategyError(ValueError):
+    """At least one ace's count is not 1 or 11."""
+
+
+class MismatchedCountingStrategyError(ValueError):
+    """The number of aces and the counts in the strategy do not match"""
+
+
 class Hand:
     def __init__(self) -> None:
         self.cards: List[str] = []
-        self.count = 0
 
     @functools.cached_property
     def card_values(cls) -> Dict[str, int]:
@@ -15,9 +28,33 @@ class Hand:
         values.update({card: 10 for card in FACE_CARDS if card != "A"})
         return values
 
-    def update_count(self, card, ace=11) -> None:
-        self.count += self.card_values[card] if card != "A" else ace
+    def count(self, aces: List[int] = None) -> int:
+        if not self.cards:
+            return 0
 
-    def add_card(self, card: str, ace=11) -> None:
+        if aces is None:
+            aces = []
+
+        if "A" in self.cards:
+            if not aces:
+                raise CountingStrategyMissingError()
+
+            if self.cards.count("A") != len(aces):
+                raise MismatchedCountingStrategyError()
+
+            if any(x not in [1, 11] for x in aces):
+                raise UnRecognizedCountingStrategyError()
+
+        values, count, ace_count = self.card_values, 0, 0
+
+        for card in self.cards:
+            if card == "A":
+                count += aces[ace_count]
+                ace_count += 1
+            else:
+                count += values[card]
+
+        return count
+
+    def add_card(self, card: str) -> None:
         self.cards.append(card)
-        self.update_count(card, ace=ace)
