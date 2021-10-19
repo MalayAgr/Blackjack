b8273986853067176a7d10cc4e904cfa19355ed8
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-10-20 04:59:12 +0530

    Simplify Hand class

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -1,60 +1,30 @@
 from __future__ import annotations
 
 import functools
-from typing import Dict, List
+from collections import UserList
+from typing import List
 
-from .deck import FACE_CARDS
-
-
-class CountingStrategyMissingError(ValueError):
-    """No counting strategy has been specified even though aces are present."""
-
-
-class UnRecognizedCountingStrategyError(ValueError):
-    """At least one ace's count is not 1 or 11."""
-
-
-class MismatchedCountingStrategyError(ValueError):
-    """The number of aces and the counts in the strategy do not match"""
+from .deck import Card
 
 
 class Hand:
     def __init__(self) -> None:
-        self.cards: List[str] = []
+        self.cards: List[Card] = []
 
-    @functools.cached_property
-    def card_values(cls) -> Dict[str, int]:
-        values = {str(card): card for card in range(2, 11)}
-        values.update({card: 10 for card in FACE_CARDS if card != "A"})
-        return values
+    def __getitem__(self, key: int) -> Card:
+        return self.cards[key]
 
-    def count(self, aces: List[int] = None) -> int:
+    def __contains__(self, item: Card) -> bool:
+        return item in self.cards
+
+    def count(self) -> int:
         if not self.cards:
             return 0
 
-        if aces is None:
-            aces = []
-
-        if "A" in self.cards:
-            if not aces:
-                raise CountingStrategyMissingError()
-
-            if self.cards.count("A") != len(aces):
-                raise MismatchedCountingStrategyError()
-
-            if any(x not in [1, 11] for x in aces):
-                raise UnRecognizedCountingStrategyError()
-
-        values, count, ace_count = self.card_values, 0, 0
+        count = 0
 
         for card in self.cards:
-            if card == "A":
-                count += aces[ace_count]
-                ace_count += 1
-            else:
-                count += values[card]
-
-        return count
+            count += card.value(current_count=count)
 
     def add_card(self, card: str) -> None:
         self.cards.append(card)
