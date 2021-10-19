b54a8d6c14837261950d1baa0eae13934aa19a75
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-10-20 04:59:53 +0530

    Add additional method and add classmethod to create player from input

--- blackjack/player.py
+++ blackjack/player.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+from rich import print
 
-from .deck import Deck
+from .deck import Card
 from .hand import Hand
 
 
@@ -9,18 +10,17 @@ class _GenericPlayer:
     def __init__(self) -> None:
         self.hand = Hand()
 
-    def player_count(self, aces=None) -> int:
-        return self.hand.count(aces=aces)
+    def player_count(self) -> int:
+        return self.hand.count()
 
-    def draw_card(self, deck: Deck):
-        card = deck.pick_card()
+    def add_card_to_hand(self, card: Card) -> None:
         self.hand.add_card(card)
 
-    def has_blackjack(self, aces=None):
-        return self.player_count(aces=aces) == 21
+    def has_blackjack(self):
+        return self.player_count() == 21
 
-    def has_busted(self, aces=None):
-        return self.player_count(aces=aces) > 21
+    def has_busted(self):
+        return self.player_count() > 21
 
 
 class Player(_GenericPlayer):
@@ -28,6 +28,27 @@ class Player(_GenericPlayer):
         self.name = name
         self.bankroll = bankroll
         super().__init__()
+
+    @classmethod
+    def from_input(cls) -> Player:
+        name = input("What should we call you? ")
+
+        print(f"[green]Hi, {name}![/green]")
+
+        while True:
+            print("\n")
+
+            try:
+                bankroll = input("How much money will you be playing with? $")
+                bankroll = float(bankroll)
+            except Exception:
+                msg = "[bold red]Oops! That's not a valid monetary value. Try again :smiley:[/bold red]"
+                print(msg)
+                continue
+
+            break
+
+        return cls(name=name, bankroll=bankroll)
 
     def pay(self, amount: float, multiplier=1):
         self.bankroll += amount * multiplier
@@ -38,6 +59,12 @@ class Player(_GenericPlayer):
 
 class Dealer(_GenericPlayer):
     def __init__(self) -> None:
-        self.face_up = ""
-        self.face_down = ""
+        self.face_up = 0
+        self.face_down = -1
         super().__init__()
+
+    def face_up_card(self) -> str:
+        return self.hand[self.face_up]
+
+    def face_down_card(self) -> str:
+        return self.hand[self.face_down]
