4c314da8c4a8b2dd738d283166a82451d0d2d87e
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-10-23 12:32:47 +0530

    Use console

--- driver.py
+++ driver.py
@@ -1,9 +1,8 @@
-from blackjack.game import Game
-from blackjack.player import Player
-
 import os
 
-from rich import print
+from blackjack.console import console
+from blackjack.game import Game
+from blackjack.player import Player
 
 
 def welcome():
@@ -14,37 +13,14 @@ def welcome():
     To know the rules and regulations of Blackjack, see https://bicyclecards.com/how-to-play/blackjack/.
     You can exit anytime by pressing CTRL + C.[/green]
     """
-    print(msg, end="\n\n\n")
-
-
-def add_player():
-    name = input("What should we call you? ")
-
-    print(f"[green]Hi, {name}![/green]")
-
-    correct = False
-
-    while not correct:
-        print("\n")
-
-        try:
-            bankroll = input("How much money will you be playing with? $")
-            bankroll = float(bankroll)
-        except Exception:
-            msg = "[bold red]Oops! That's not a valid monetary value. Try again :smiley:[/bold red]"
-            print(msg)
-            continue
-
-        correct = True
-
-    return Player(name=name, bankroll=bankroll)
+    console.print(msg, end="\n\n\n")
 
 
 def main():
     try:
         welcome()
 
-        player = add_player()
+        player = Player.from_input()
 
         input("Press ENTER to start playing.")
         os.system("cls||clear")
